83cdd1174a31c24d9ee3914fe2e2d5296756ebdc
Nishant Raj <nishantraj2109@gmail.com>
2024-05-08 02:05:53 +0530

    app.js file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,24 @@
-import React from 'react'
-import Navbar from './components/Navbar'
-import HomePage from './components/Homepage'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import HomePage from "./components/Homepage";
+import CreateRecipe from "./components/CreateRecipe";
+import Favourite from "./components/Favourite";
+import Login from "./components/Login";
+import Singup from "./components/Singup";
+// import AboutPage from './components/AboutPage';
+// import ContactPage from './components/ContactPage';
 
 const App = () => {
   return (
-    <>
-      <Navbar/>
-      <HomePage/>
-    </>
-  )
-}
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/create" element={<CreateRecipe />} />
+      <Route path="/favourite" element={<Favourite />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Singup />} />
+    </Routes>
+  );
+};
 
-export default App
+export default App;
