e2c8288c8dcf3332d42cb5d16f4e16687557c585
Nishant Raj <nishantraj2109@gmail.com>
2024-05-08 02:03:43 +0530

    make loader

---
+++ frontend/src/components/Loader.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Loader.css"; // Assuming you have a CSS file named Loader.css for styling
+
+const Loader = () => {
+  return <div className="loader"></div>;
+};
+
+export default Loader;
